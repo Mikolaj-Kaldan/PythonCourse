863a6b3a6dea4db689109229c5c20932d2f2720d
Mikolaj-Kaldan <163213599+Mikolaj-Kaldan@users.noreply.github.com>
2024-03-19 01:05:59 +0100

    Update 4E1.py

--- 4E1.py
+++ 4E1.py
@@ -1,2 +1,7 @@
-p = ((0.8, 0.3), (0.6, 0.7), (0.8, 0.7), (0.7, 1.0), (0.1, 0.1))
+p = [(-0.8, -0.3), (0.6, 0.7), (0.8, 0.7), (0.7, -1.0), (-0.1, 0.1)]
 print(list(filter((lambda p: (False,True)[pow(p[0],2) + pow(p[1],2) <= 1]),p)))
+print(list(filter((lambda p: (False,True)[p[0] + p[1] > 0 and p[0]*p[1] > 0]),p)))
+p.sort(key = lambda p: (-p[1],p[0]))
+print(p)
+p.sort(key = lambda p: abs(p[0])+abs(p[1]))
+print(p)
