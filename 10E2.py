565e86a48c1cae539f262217933fb33a45ac59c0
Mikolaj-Kaldan <163213599+Mikolaj-Kaldan@users.noreply.github.com>
2024-06-09 21:41:32 +0200

    Create 10E2.py

---
+++ 10E2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dates = np.arange('2024-05-01','2024-06-01',dtype='datetime64[D]')
+gold_rate = [np.random.randint(2300, 2400)]
+for x in range(len(dates)-1):
+    gold_rate.append(gold_rate[x]+np.random.randint(-10, 10))
+series = pd.Series(data = gold_rate, index = dates)
+series.plot()
+plt.xlabel("Date")
+plt.ylabel("Gold rate [USD/oz]")
+plt.show()
